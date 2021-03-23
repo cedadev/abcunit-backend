23217cc671941030e61974ce476b96a6bd872f00
Jonathan Haigh <jhaigh0@sci5.jasmin.ac.uk>
2021-03-23 11:05:52 +0000

    docs update

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,26 @@
 
-extensions = [
-    'm2r'
-]
+import sys, os
+
+project = 'ABCUnit Backend'
+copyright = '2020, Jonathan Haigh'
+author = 'Jonathan Haigh'
+
+version = '1.2.0'
+release = 'v1.2.0'
+
+highlight_language = 'python'
+pygments_style = 'sphinx'
+
+
+extensions = ['m2r']
 
 source_suffix = ['.rst', '.md']
+
+master_doc = "index"
+
+exclude_patterns = ['_build', 'sphinx-enki-info.txt']
+
+keep_warnings = True
+
+
+html_theme = 'default'
